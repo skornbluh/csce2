9915d055240a5a77a20345a5d7099eca0105edc0
Austin Connor <aconnor731@gmail.com>
2017-05-19 11:30:52 -0500

    Add files via upload

---
+++ Child.php
@@ -0,0 +1,25 @@
+<?php
+
+class Child {
+
+  private $name;
+  private $age;
+
+  public function __construct($name, $age) {
+    $this->name = $name;
+    $this->age  = $age;
+  } 
+
+  public function getName() {
+    return $this->name;
+  }
+
+  public function getAge() {
+    return $this->age;
+  }
+
+  public function __toString() {
+    return $this->name . " (" . $this->age . ")";
+  }
+
+}
